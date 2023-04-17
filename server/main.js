b57bfc23fd7567e0273f5e55c17eb97041fc6b06
Errera11 <forvitinn@mail.ru>
2023-04-17 18:41:43 +0300

    add sync canvas onopen

--- server/main.js
+++ server/main.js
@@ -2,7 +2,13 @@ require('dotenv').config({path: './.env'})
 const app = require('express')();
 const expressWs = require('express-ws')(app);
 const Wss = expressWs.getWss();
+const fs = require('fs')
+const path = require('path')
+const cors = require('cors')
+const express = require('express')
 
+app.use(cors())
+app.use(express.json())
 const PORT=process.env.PORT;
 app.ws('/', (ws, req) => {
     ws.on('message', msg => {
@@ -16,6 +22,15 @@ app.ws('/', (ws, req) => {
                 break
         }
     })
+})
+
+app.post('/image', (req, res) => {
+    fs.writeFileSync(path.resolve(__dirname, "images", req.query.id + '.jpg'), req.body.image, 'base64')
+})
+
+app.get('/image', (req, res) => {
+    const data = fs.readFileSync(path.resolve(__dirname, "images", req.query.id + '.jpg'))
+    res.json(data.toString('base64'));
 })
 
 const connectionHandler = (ws, message) => {
